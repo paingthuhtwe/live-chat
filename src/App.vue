efa4b5ff36d9f8237e733e3ae6e4a879267f2a56
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-05-16 15:57:39 +0630

    project setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <WelcomePage></WelcomePage>
 </template>
+
+<script>
+import WelcomePage from "./views/WelcomePage.vue";
+export default {
+  components: {
+    WelcomePage,
+  },
+  setup() {},
+};
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
---
+++ src/views/WelcomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="w-full md:w-[720px] lg:w-[1080px] mx-auto bg-white mt-12 rounded-lg shadow-sm p-3"
+  >
+    <h1 class="text-center text-3xl font-bold py-5">Project Setup</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
