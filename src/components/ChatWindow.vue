48d8bd7586b4529f358aa9f3847441bf91af53e6
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-05-17 15:51:55 +0630

    add auto scroll after send message

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,7 +1,8 @@
 <template>
   <div
-    class="w-[100%] h-[80vh] md:h-[65vh] overflow-y-auto scroll-hide"
+    class="w-[100%] h-[80vh] md:h-[65vh] overflow-y-auto bg-slate-50 border-b border-indigo-100"
     v-if="user"
+    ref="chatWindow"
   >
     <div v-for="message in messages" :key="message">
       <ChatMessage :message="message" :user="user"></ChatMessage>
@@ -13,14 +14,23 @@
 import ChatMessage from "./ChatMessage";
 import getMessages from "../composables/getMessages";
 import getUser from "../composables/getUser";
+import { onUpdated, ref } from "vue";
 
 export default {
   components: { ChatMessage },
   setup() {
     let { messages } = getMessages();
     let { user } = getUser();
+    let chatWindow = ref(null);
 
-    return { messages, user };
+    // auto scrolling feature
+    onUpdated(() => {
+      if (chatWindow.value) {
+        chatWindow.value.scrollTop = chatWindow.value.scrollHeight;
+      }
+    });
+
+    return { messages, user, chatWindow };
   },
 };
 </script>
